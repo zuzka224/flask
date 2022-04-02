9e0f3e7f5ad367728ee243cd7715c1f8e4d5c862
Zuzka Neupaurová <15zuzi25@gmail.com>
2022-04-02 19:30:01 +0200

    synchronizacia py, html, js, css

--- helloFlask/komponenty/app.py
+++ helloFlask/komponenty/app.py
@@ -1,6 +1,8 @@
+from distutils.log import debug
+from unicodedata import name
 from PIL import Image
 from random import randint
-from flask import Flask, send_file, request
+from flask import Flask, send_file, request, redirect, url_for, render_template
 from io import BytesIO
 app = Flask(__name__)
 
@@ -13,40 +15,11 @@ def serve_pil_image(img):
 	return send_file(img_io, mimetype='image/jpeg')
 
 
-zakladna_stranka = """
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Vector Script GUI</title>
 
-	<link rel="stylesheet" type="text/css" href="style.css">
+@app.route('/')
+def home():
+ return render_template("index.html")
 
 
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css"
-		integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous" />
-</head>
-<body>
-<header id="header">VECTOR SCRIPT GUI</header>
-<section id="form_text">
-	<form method="POST" action="/render">
-		
-		<textarea name="ves"></textarea>
-		<button class="btn">Zobraziť</button>
-	</form>
-</section>
-<section>
-	<img id="output" />
-</section>
-<script type="text/javascript" src="script.js" ></script>
-</body>
-</html>
-"""
-
-
-
-@app.route('/', methods=['GET', 'POST'])
-def base():
- return zakladna_stranka
-
-base()
+if __name__ == "__main__":
+	app.run(debug=True)
