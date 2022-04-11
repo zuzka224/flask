decb020e3e6c88466e3f3a72ade26a5ad4655f85
Zuzka Neupaurová <15zuzi25@gmail.com>
2022-04-11 19:11:22 +0200

    part 2

--- helloFlask/komponenty/static/script.js
+++ helloFlask/komponenty/static/script.js
@@ -37,3 +37,69 @@ function type() {
 document.addEventListener("DOMContentLoaded", function () {
 	type();
 });
+
+
+function clear() {
+	// When clear button is clicked
+
+	let color = "#FFFFFF";
+	let text = "VES v1.4 600 400\n"
+	let columns = text.split('\n')
+	let background = columns[0] + "\n" + "CLEAR " + color;
+	document.querySelector("#ves").value = background
+	document.getElementById("vykresli").click();
+
+}
+
+
+function download_image(linkElement) {
+	// When download button is clicked
+	let source = document.getElementById("output").src;
+	linkElement.href = source
+}
+
+/*FOOTER */
+
+const gl = document.getElementById("canvas").getContext("webgl");
+const programInfo = twgl.createProgramInfo(gl, ["vertexShader", "fragmentShader"]);
+
+const arrays = {
+	position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
+};
+const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+
+let mouseX = 0, mouseY = 0;
+
+document.getElementById("canvas").addEventListener('mousemove', e => {
+	mouseX = e.clientX;
+	mouseY = e.clientY;
+});
+
+
+function render(time) {
+	//Paints only 25% pixels in the screen space, 
+	//Slightly lower resolution but much better performance!
+	//See also: the zoom feature of https://glslsandbox.com/
+
+	twgl.resizeCanvasToDisplaySize(gl.canvas, 0.5);
+
+	//Paints 4x the number of pixels.
+	//Very computationally expensive in full screen on desktop devices
+	//twgl.resizeCanvasToDisplaySize(gl.canvas, 1.0); 
+
+	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+	const uniforms = {
+		u_time: (time) * 0.002,
+		u_resolution: [gl.canvas.width, gl.canvas.height],
+		u_mouse: [mouseX, mouseY],
+	};
+
+	gl.useProgram(programInfo.program);
+	twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+	twgl.setUniforms(programInfo, uniforms);
+	twgl.drawBufferInfo(gl, bufferInfo);
+
+	requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
